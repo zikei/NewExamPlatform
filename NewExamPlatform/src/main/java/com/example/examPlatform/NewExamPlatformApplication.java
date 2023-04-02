1d3347a6a273f85051a868a049cbb8988b408956
zikei <sigetaka1517@gmail.com>
2023-04-02 12:52:01 +0900

    新規プロジェクト作成

---
+++ NewExamPlatform/src/main/java/com/example/examPlatform/NewExamPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.example.examPlatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewExamPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewExamPlatformApplication.class, args);
+	}
+
+}
---
+++ NewExamPlatform/src/test/java/com/example/examPlatform/NewExamPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.examPlatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewExamPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
