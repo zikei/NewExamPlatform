d851575927b63937b0ad5ddf37c68fd56b04061e
zikei <sigetaka1517@gmail.com>
2023-04-22 19:14:33 +0900

    ログイン後の画面遷移先パスを修正

--- NewExamPlatform/src/main/java/com/example/examPlatform/config/WebSecurityConfig.java
+++ NewExamPlatform/src/main/java/com/example/examPlatform/config/WebSecurityConfig.java
@@ -4,6 +4,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -15,14 +17,18 @@ import org.springframework.security.web.SecurityFilterChain;
 public class WebSecurityConfig {
 	
 	@Bean
-	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {http
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+	    String name = auth.getName();
+	    
+		http
 			.authorizeHttpRequests((requests) -> requests
 				.requestMatchers("/ExamPlatform/Home").permitAll()
 				.anyRequest().authenticated()
 			)
 			.formLogin((form) -> form
 				.loginPage("/ExamPlatform/Login")
-				.defaultSuccessUrl("/ExamPlatform/")
+				.defaultSuccessUrl("/ExamPlatform/" + name)
 				.permitAll()
 			)
 			.logout((logout) -> logout.permitAll());
