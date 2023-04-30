8372aea71871824be2c90ad6770985741bcb8099
zikei <sigetaka1517@gmail.com>
2023-04-30 19:43:10 +0900

    正規表現チェックのエラーメッセージ追加

--- NewExamPlatform/src/main/java/com/example/examPlatform/form/AccountEntryForm.java
+++ NewExamPlatform/src/main/java/com/example/examPlatform/form/AccountEntryForm.java
@@ -11,11 +11,11 @@ public class AccountEntryForm {
 	/** ユーザ名 半角英数字*/
 	@NotBlank
 	@Size(min=2, max=30)
-	@Pattern(regexp="^[a-zA-Z0-9]+$")
+	@Pattern(regexp="^[a-zA-Z0-9]+$", message="ユーザ名は半角英数字で入力してください")
 	private String userName;
 	
 	/** パスワード 半角英数字記号*/
 	@NotBlank
-	@Pattern(regexp="^[!-~]+$")
+	@Pattern(regexp="^[!-~]+$", message="パスワードは半角英数字記号で入力してください")
 	private String password;
 }
