dca93b5e2b4fdf1d2c9ab6ce65ff3d1cc8151b09
zikei <sigetaka1517@gmail.com>
2023-04-30 18:48:03 +0900

    アカウント登録後の自動ログイン処理の修正

--- NewExamPlatform/src/main/java/com/example/examPlatform/service/AccountService.java
+++ NewExamPlatform/src/main/java/com/example/examPlatform/service/AccountService.java
@@ -5,5 +5,5 @@ import com.example.examPlatform.entity.Account;
 /** アカウント関連処理　*/
 public interface AccountService {
 	/** ユーザ登録 */
-	Account userRegister(Account registUser);
+	void userRegister(Account registUser);
 }
