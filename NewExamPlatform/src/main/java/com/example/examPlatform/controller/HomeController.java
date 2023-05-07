adc482e11b349edefa335095f913fb7b7a5c9fcc
zikei <sigetaka1517@gmail.com>
2023-05-08 00:22:41 +0900

    ホームコントローラ作成

--- NewExamPlatform/src/main/java/com/example/examPlatform/controller/HomeController.java
+++ NewExamPlatform/src/main/java/com/example/examPlatform/controller/HomeController.java
@@ -1,20 +1,35 @@
 package com.example.examPlatform.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.examPlatform.entity.Exam;
+import com.example.examPlatform.service.HomeService;
 
 /** ホームコントローラ */
 @Controller
 @RequestMapping("/ExamPlatform")
 public class HomeController {
+	@Autowired
+	HomeService homeService;
+	
 	/** ホーム画面 */
 	@GetMapping(value= { "/","Home"})
-	public String Home() {
+	public String Home(Model model) {
+		List<Exam> newExamList = homeService.selectNewArrivalsExams();
+		List<Exam> ExeTopExamList = homeService.selectMonthlyExeTopExams();
+		List<Exam> BookmarkTopExamList = homeService.selectBookmarkTopExams();
 		
-		
+		model.addAttribute("newExamList", newExamList);
+		model.addAttribute("exeTopExamList", ExeTopExamList);
+		model.addAttribute("bookmarkTopExamList", BookmarkTopExamList);
 		return "home";
 	}
 	
