8c65a7a695b87aaa0769f39500c5755053b44988
zikei <sigetaka1517@gmail.com>
2023-06-03 20:01:03 +0900

    試験登録機能実装

--- NewExamPlatform/src/main/java/com/example/examPlatform/service/ExamService.java
+++ NewExamPlatform/src/main/java/com/example/examPlatform/service/ExamService.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import com.example.examPlatform.data.link.ExamLinkView;
+import com.example.examPlatform.data.question.BigQuestionData;
 import com.example.examPlatform.entity.Exam;
 
 /** 試験関連処理 */
@@ -16,6 +17,9 @@ public interface ExamService {
 	
 	/** ユーザIDで試験取得 */
 	List<Exam> selectExamByUserID(Integer userId);
+	
+	/** 試験登録 */
+	void examRegister(Exam exam, List<BigQuestionData> questionData);
 	
 	/** 試験リストを試験リンクリスト形式に変換*/
 	List<ExamLinkView> makeExamLinkList(List<Exam> examList);
--- NewExamPlatform/src/main/java/com/example/examPlatform/service/ExamServiceImpl.java
+++ NewExamPlatform/src/main/java/com/example/examPlatform/service/ExamServiceImpl.java
@@ -9,8 +9,16 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.example.examPlatform.data.link.ExamLinkView;
+import com.example.examPlatform.data.question.BigQuestionData;
+import com.example.examPlatform.data.question.QuestionData;
+import com.example.examPlatform.entity.BigQuestion;
+import com.example.examPlatform.entity.Choices;
 import com.example.examPlatform.entity.Exam;
+import com.example.examPlatform.entity.Question;
+import com.example.examPlatform.repository.BigQuestionRepository;
+import com.example.examPlatform.repository.ChoicesRepository;
 import com.example.examPlatform.repository.ExamRepository;
+import com.example.examPlatform.repository.QuestionRepository;
 
 /** 試験関連処理実装クラス　*/
 @Service
@@ -18,6 +26,15 @@ import com.example.examPlatform.repository.ExamRepository;
 public class ExamServiceImpl implements ExamService{
 	@Autowired
 	ExamRepository examRepo;
+	
+	@Autowired
+	BigQuestionRepository bqRepo;
+	
+	@Autowired
+	QuestionRepository qRepo;
+	
+	@Autowired
+	ChoicesRepository cRepo;
 	
 	/** 試験全件取得 */
 	@Override
@@ -41,6 +58,13 @@ public class ExamServiceImpl implements ExamService{
 		examRepo.findByUserId(userId).forEach(examList::add);
 		return examList;
 	}
+	
+	@Override
+	public void examRegister(Exam exam, List<BigQuestionData> questionData) {
+		Exam registExam = insertExam(exam);
+		Integer examId = registExam.getExamId();
+		insertBigQuestionData(examId, questionData);
+	}
 
 	@Override
 	public List<ExamLinkView> makeExamLinkList(List<Exam> examList) {
@@ -51,5 +75,57 @@ public class ExamServiceImpl implements ExamService{
 			examLinkList.add(addLink);
 		}
 		return examLinkList;
+	}
+	
+	/** DBに試験概要を保存 */
+	private Exam insertExam(Exam exam) {
+		exam.setExamId(null);
+		return examRepo.save(exam);
+	}
+	
+	/** DBに大問、小問、選択肢を保存 */
+	private void insertBigQuestionData(Integer examId, List<BigQuestionData> bqDataList) {
+		for(BigQuestionData bqData : bqDataList) {
+			BigQuestion registBq = insertBigQuestion(examId, bqData.getBigQuestion());
+			Integer bqId = registBq.getBigQuestionId();
+			insertQuestionData(bqId, bqData.getQuestionList());
+		}
+	}
+	
+	/** DBに小問、選択肢を保存 */
+	private void insertQuestionData(Integer bqId, List<QuestionData> qDataList) {
+		for(QuestionData qData : qDataList) {
+			Question registQ = insertQuestion(bqId, qData.getQuestion());
+			Integer qId = registQ.getQuestionId();
+			insertChoicesList(qId, qData.getChoicesList());
+		}
+	}
+	
+	/** DBに選択肢リストを保存 */
+	private void insertChoicesList(Integer bqId, List<Choices> cList) {
+		for(Choices c : cList) {
+			insertChoices(bqId, c);
+		}
+	}
+	
+	/** DBに大問を保存 */
+	private BigQuestion insertBigQuestion(Integer examId, BigQuestion bq) {
+		bq.setBigQuestionId(null);
+		bq.setExamId(examId);
+		return bqRepo.save(bq);
+	}
+	
+	/** DBに小問を保存 */
+	private Question insertQuestion(Integer bqId, Question q) {
+		q.setQuestionId(null);
+		q.setBigQuestionId(bqId);
+		return qRepo.save(q);
+	}
+	
+	/** DBに選択肢を保存 */
+	private Choices insertChoices(Integer bqId, Choices c) {
+		c.setChoicesId(null);
+		c.setQuestionId(bqId);
+		return cRepo.save(c);
 	}
 }
