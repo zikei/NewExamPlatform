d72e36b723a529969de7d4c3bb53544a7b1041f7
zikei <sigetaka1517@gmail.com>
2023-06-22 16:55:56 +0900

    試験IDでタグを削除する

--- NewExamPlatform/src/main/java/com/example/examPlatform/repository/TagRepository.java
+++ NewExamPlatform/src/main/java/com/example/examPlatform/repository/TagRepository.java
@@ -16,4 +16,8 @@ public interface TagRepository extends CrudRepository<Tag, Integer> {
 	/** 重複なしの全タグを取得 */
 	@Query("SELECT DISTINCT Tag FROM Tag")
 	Iterable<String> findTag();
+	
+	/** 指定された試験のタグを削除 */
+	@Query("DELETE FROM Tag WHERE ExamId = :examId")
+	void deleteByExamId(Integer examId);
 }
