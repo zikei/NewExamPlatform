612fab7c41d4d76f74a74713b3282b1953b04b8d
zikei <sigetaka1517@gmail.com>
2023-07-24 23:05:12 +0900

    試験解答フォーム

--- NewExamPlatform/src/main/java/com/example/examPlatform/form/ExamActForm.java
+++ NewExamPlatform/src/main/java/com/example/examPlatform/form/ExamActForm.java
@@ -4,16 +4,27 @@ import java.util.ArrayList;
 import java.util.List;
 
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 /** 試験受験Form */
 @Data
 public class ExamActForm {
+	/** やり直し試験 */
+	@NotNull
+	private boolean isRedo;
+	
 	/** 解答リスト */
 	@Valid
 	private List<AnsForm> ansList;
 	
 	public ExamActForm() {
+		this.isRedo = false;
+		ansList = new ArrayList<>();
+	}
+	
+	public ExamActForm(boolean isRedo) {
+		this.isRedo = isRedo;
 		ansList = new ArrayList<>();
 	}
 	
